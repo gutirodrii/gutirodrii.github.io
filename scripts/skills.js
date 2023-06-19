1b5b3df75717b8e1fdca8b9994e0f27d443a0b44
gutirodrii <gutirodriidev@gmail.com>
2023-06-19 15:38:00 +0200

    delete console.logs

--- scripts/skills.js
+++ scripts/skills.js
@@ -4,8 +4,5 @@ function resetButtonState(){
     const trueState = db.skills.find((skill) => skill.state === true)
     if(trueState){
       trueState.state = false;
-      console.log('Estado cambiado a false')
-    }else{
-      console.log('No hay truestate')
     }
   }
