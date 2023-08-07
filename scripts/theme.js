5a5663d408e933987dce62fd658b079f3d918739
gutirodrii <gutirodriidev@gmail.com>
2023-08-07 13:03:30 +0200

    dark-mode-default

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,6 +1,11 @@
 const darkmode = document.getElementById('darkmode-toggle');
 darkmode.addEventListener('click', toggleDarkMode);
 
+
+if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  toggleDarkMode();
+  darkmode.checked = true;
+}
 function toggleDarkMode() {
   const body = document.body;
   body.classList.toggle('dark-mode');
