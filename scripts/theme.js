65ec43c7d7017ec62513c1efbed8b3351be82e66
gutirodrii <gutirodriidev@gmail.com>
2023-08-08 11:53:01 +0200

    empty

--- scripts/theme.js
+++ scripts/theme.js
@@ -5,6 +5,7 @@ darkmode.addEventListener('click', toggleDarkMode);
 if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   toggleDarkMode();
   darkmode.checked = true;
+  //
 }
 function toggleDarkMode() {
   const body = document.body;
